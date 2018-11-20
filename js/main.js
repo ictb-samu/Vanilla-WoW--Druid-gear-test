b5dab42c4c05c9b25e32f158dfd89ed78c68bdd4
ictb-samu <43340495+ictb-samu@users.noreply.github.com>
2018-11-20 19:16:07 +0200

    testing new stuff

--- js/main.js
+++ js/main.js
@@ -4,18 +4,67 @@ let headSelect = document.getElementById("head");
 let headOption = head.options[head.selectedIndex].value;
 */
 
-// Head item database below
+// Helmets
 let headItems = { 
-  r13Head: {arm: 192, ap: 0, str: 20, agi: 19, sta: 19, int: 15, crit: 1.0, sp: 18},
-  genesisHelm: {arm: 192, ap: 0, str: 15, agi: 12, sta: 22, int: 22, crit: 1.0, sp: 27}
+  r13Head: {arm: 192, ap: 0, str: 20, agi: 19, sta: 19, int: 15, crit: 1.0, sp: 18, scrit: 0, spirit: 0},
+  genesisHelm: {arm: 192, ap: 0, str: 15, agi: 12, sta: 22, int: 22, crit: 1.0, sp: 27, scrit: 0, spirit: 0}
 };
-// Neck item database
+
+// Necklaces
 let neckItems = {
-  prestors: {arm: 0, ap: 0, str: 0, agi: 30, sta: 0, int: 0, crit: 0, sp: 0},
-  stormragesTalisman: {arm: 0, ap: 26, str: 0, agi: 0, sta: 12, int: 0, crit: 2, sp: 0}
+  prestors: {arm: 0, ap: 0, str: 0, agi: 30, sta: 0, int: 0, crit: 0, sp: 0, scrit: 0, spirit: 0},
+  stormragesTalisman: {arm: 0, ap: 26, str: 0, agi: 0, sta: 12, int: 0, crit: 2, sp: 0, scrit: 0, spirit: 0}
 };
 
+// Shoulders
+let shoulderItems = {
+  wickedRev: {arm: 1, ap: 2, str: 3, agi: 4, sta: 5, int: 6, crit: 7, sp: 8, scrit: 0, spirit: 0}
+};
 
+// 
+let Items = {
+  : {arm: , ap: , str: , agi: , sta: , int: , crit: , sp: },
+  : {arm: , ap: , str: , agi: , sta: , int: , crit: , sp: }
+};
+
+let headSelect, neckSelect, shoulderSelect, backSelect, chestSelect, wristSelect, glovesSelect,
+    beltSelect, legsSelect, feetSelect, ring1Select, ring2Select, trinket1Select, trinket2Select;
+    headSelect = document.getElementById("head").value;
+    neckSelect = document.getElementById("neck").value;
+    shoulderSelect = document.getElementById("shoulder").value;
+    backSelect = document.getElementById("back").value;
+    chestSelect = document.getElementById("chest").value;
+    wristSelect = document.getElementById("wrist").value;
+    glovesSelect = document.getElementById("gloves").value;
+    beltSelect = document.getElementById("belt").value;
+    legsSelect = document.getElementById("legs").value;
+    feetSelect = document.getElementById("feet").value;
+    ring1Select = document.getElementById("ring1").value;
+    ring2Select = document.getElementById("ring2").value;
+    trinket1Select = document.getElementById("trinket1").value;
+    trinket2Select = document.getElementById("trinket2").value;
+
+let head, neck, shoulder, back, chest, wrist, gloves, belt, legs, feet, ring1, ring2, trinket1, trinket2;
+    head = headItems[headSelect];
+    neck = neckItems[neckSelect];
+    shoulder = shoulderItems[shoulderSelect];
+    back = backItems[backSelect];
+    chest = chestItems[chestSelect];
+    wrist = wristItems[wristSelect];
+    gloves = glovesItems[glovesSelect];
+    belt = beltItems[beltSelect];
+    legs = legsItems[legsSelect];
+    feet = feetItems[feetSelect];
+    ring1 = ring1Items[ring1Select];
+    ring2 = ring2Items[ring2Select];
+    trinket1 = trinket1Items[trinket1Select];
+    trinket2 = trinket2Items[trinket2Select];
+
+agility = neck.agi + head.agi;
+critChance = agility / 20 + neck.crit + head.crit;
+
+
+/*
 // Get head item and change it based on selection
 // print for test purpose
 let headSelect = document.getElementById("head").value;
@@ -23,8 +72,10 @@ let head = headItems[headSelect];
 function getHeadOption() {
   headSelect = document.getElementById("head").value;
   head = headItems[headSelect];
-  console.log(headSelect);
-  console.log(head);
+  agility = neck.agi + head.agi;
+  critChance = agility / 20 + neck.crit + head.crit;
+  document.getElementById("crit").innerHTML = "Crit chance: " + (critChance) + "%";
+  document.getElementById("agility").innerHTML = "Agility: " + (agility);
 }
 
 // Get neck item and change it based on selection
@@ -34,18 +85,18 @@ let neck = neckItems[neckSelect];
 function getNeckOption() {
   neckSelect = document.getElementById("neck").value;
   neck = neckItems[neckSelect];
-  console.log(neckSelect);
-  console.log(neck);
+  agility = neck.agi + head.agi;
+  critChance = agility / 20 + neck.crit + head.crit;
 }
 
-console.log(neck);
-console.log(head);
 
-let agility = neck.agi + head.agi;
-let critChance = agility / 20 + neck.crit + head.crit;
+let agility;
+let critChance;
 
-console.log(agility);
-console.log(critChance);
+
+document.getElementById("crit").innerHTML = "Crit chance: " + (critChance) + "%";
+document.getElementById("agility").innerHTML = "Agility: " + (agility);
+
 /* This code maybe obsolete soon
 
 let neckSelect = document.getElementById("neck");
@@ -53,46 +104,3 @@ let neckOption = neck.options[neck.selectedIndex].value;
 */
 
 
-
-let shoulder = document.getElementById("shoulder");
-let shoulderVal = shoulder.options[shoulder.selectedIndex].value;
-
-
-let back = document.getElementById("back");
-let backVal = back.options[back.selectedIndex].value;
-
-
-let chest = document.getElementById("chest");
-let chestVal = chest.options[chest.selectedIndex].value;
-
-
-let wrist = document.getElementById("wrist");
-let wristVal = wrist.options[wrist.selectedIndex].value;
-
-
-let gloves = document.getElementById("gloves");
-let glovesVal = gloves.options[gloves.selectedIndex].value;
-
-
-let legs = document.getElementById("legs");
-let Val = legs.options[legs.selectedIndex].value;
-
-
-let feet = document.getElementById("feet");
-let feetVal = feet.options[feet.selectedIndex].value;
-
-
-let ring1 = document.getElementById("ring1");
-let ring1Val = ring1.options[ring1.selectedIndex].value;
-
-
-let ring2 = document.getElementById("ring2");
-let ring2Val = ring2.options[ring2.selectedIndex].value;
-
-
-let trinket1 = document.getElementById("trinket1");
-let trinket1Val = trinket1.options[trinket1.selectedIndex].value;
-
-
-let trinket2 = document.getElementById("trinket2");
-let trinket2Val = trinket2.options[trinket2.selectedIndex].value;
